0a2074a4a58dc31f77bec1f72eea2422301fd490
pgflorczak <70096405+pgflorczak@users.noreply.github.com>
2020-10-31 19:00:49 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package brickBraker;
+import javax.swing.JFrame;
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		Gameplay gamePlay = new Gameplay();
+		obj.setBounds(10, 10, 700,600);
+		obj.setTitle("Arkanoid");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+		obj.setLocationRelativeTo(null);
+	}
+}
